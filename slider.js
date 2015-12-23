129f96e0c3ec732cd8d5b64a941e401e7edca207
tonydong <467598557@qq.com>
2015-12-23 17:56:27 +0800

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,3 +1,12 @@
+/**
+@param wrapper 内容滑动容器(原生的dom对象)
+@param onSliderStartCallback  开始滑动时回调函数，返回true表示继续执行，返回false表示不继续执行下面的内容
+@param onSliderEndCallback  滑动结束后回调函数
+@param maxSliderLength  用户最长可以滑动的长度(px)
+@param minSliderLength  用户最小可以触发onSliderEndCallback的滑动长度
+@param sliderBackDuration 松开手之后，容器回归动画时间
+@param delayToSliderBackDuration  松开手之后，容器回归之前，需要等待的时间
+*/
 (function() {
 	function Slider(e) {
 		this.e = e;
